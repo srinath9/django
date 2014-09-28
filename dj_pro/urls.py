874a043134d449ee9bacf969749df78e7d0b3a26
srinath9 <ksrianthchowdary9@gmail.com>
2014-09-29 01:15:44 +0530

    change in url pattern

--- dj_pro/urls.py
+++ dj_pro/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^$','products.views.list_all', name ="all_products"),
-	url(r'^products/', include(products.urls)),
-	url(r'^products/(?P<slug>.*)$','products.views.single', name ="product"),
+	#url(r'^$','products.views.list_all', name ="all_products"),
+	url(r'^products/', include('products.urls')),
+	
     # Examples:
     # url(r'^$', 'dj_pro.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
